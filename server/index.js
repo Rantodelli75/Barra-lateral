209f0f771f11554ce95a70e878b144e917336eb5
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2023-09-23 17:02:03 -0400

    server y login terminado

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mariadb = require("mariadb/callback");
 const cors = require("cors");
 
 const app = express();
@@ -7,24 +7,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const con = mysql.createConnection({
+const pool = mariadb.createConnection ({
     user: "admin",
     host: "localhost",
     password: "12345678",
-    database: "crud"
-})
+    database: "sice3"
+});
+
 app.post("/login", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    con.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], 
+    pool.query("SELECT username,password FROM usuarios WHERE username = ? AND password = ?", [username, password], 
         (err, result) => {
+            
             if(err){
                 req.setEncoding({err: err});
             }else{
                 if(result.length > 0){
                     res.send(result);
                 }else{
-                    res.send({message: "WRONG USERNAME OR PASSWORD!"})
+                    res.send({message: "DATOS EQUIVOCADOS!"})
                 }
             }
         }
--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import "./estilos/codigo.css";
 import logo from "./estilos/imagenes/logo.jpeg";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+  const login = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if(response.data.message){
+        setLoginStatus(response.data.message);
+      }else{
+        setLoginStatus(response.data[0].username);
+      }
+    })
+  }
   return (
     <>
-      <h1 class="Title">HUMBOLDT CRUD</h1>
-      <div class="login-box">
+      <h1 className="Title">HUMBOLDT CRUD</h1>
+      <div className="login-box">
         <center>
-          <img src={logo} class="Logo" alt="Logo sice" />
+          <img src={logo} className="Logo" alt="Logo sice" />
         </center>
         <h1>Iniciar Sesión</h1>
         <form>
-          <input type="text" placeholder="Usuario o Email" />
-          <input type="password" placeholder="Contraseña" />
-          <input type="submit" value="Ingresar" />
+          <input type="text" placeholder="Usuario" onChange={(e) => {setUsername(e.target.value)}}/>
+          <input type="password" placeholder="Contraseña"  onChange={(e) => {setPassword(e.target.value)}}/>
+          <input type="submit" onClick={login} value="Ingresar" />
+          <h1 style={{color: 'red'}}>Aqui {loginStatus}</h1>
           <a href="#">¿Olvidó su contraseña?</a>
           <br />
         </form>
       </div>
-      <div class="Novedades">
+      <div className="Novedades">
         <img src="" alt="" />
       </div>
     </>
