e6fe407f08ecc7391a0e7877ca096e4188a387df
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2023-09-23 09:08:59 -0400

    inicial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const con = mysql.createConnection({
+    user: "admin",
+    host: "localhost",
+    password: "12345678",
+    database: "crud"
+})
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    con.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], 
+        (err, result) => {
+            if(err){
+                req.setEncoding({err: err});
+            }else{
+                if(result.length > 0){
+                    res.send(result);
+                }else{
+                    res.send({message: "WRONG USERNAME OR PASSWORD!"})
+                }
+            }
+        }
+    )
+})
+
+app.listen(3001, () => {
+    console.log("running backend server");
+})
